9368123a80bc29907d1b6c81465bd37083c63654
Gabi <gabrielcorrieri1@gmail.com>
2021-11-11 20:09:49 -0300

    prueba

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import Button from "../Button/Button"
+
+const NavBar = (props) => {
+console.log(props)
+  let miVariable = 'Home'
+
+  const miFuncion = () => {
+    miVariable = 'About'
+  }
+    return (
+      <nav className= 'NavBar'>
+        <div className='LeftNav'>
+          <div className='navOptionsLefts'>
+            <Container user='anonimo'>
+              <miButton className= 'Option' label={miVariable} loquequiera={miFuncion}/>
+            </Container>
+            <button className='Option'> Favoritos</button>
+          </div>
+        </div>
+        <div className='RightNav'>
+        <div className='navOptionsLefts'>
+        <button className='Option'> Singup</button>
+        </div>
+        </div>
+        <CartWidget.js/>
+        </nav>
+    )
+  }
+
+  export default NavBar
